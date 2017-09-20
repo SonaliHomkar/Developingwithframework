0d221b171d63b9bc2c02b6421eb101260a04085e
snail.hirve <sonail.hirve@gmail.com>
2017-09-20 15:28:26 +0530

    updated for facebook login and lgout

--- project.py
+++ project.py
@@ -89,7 +89,7 @@ def newMenuItem(restaurant_id):
                                    restaurant_id=restaurant_id,
                                    description = request.form['txtdescription'],
                                    price = request.form['txtprice'],
-                                   user_id=restaurant_id.user_id,
+                                   user_id = login_session['user_id'],
                                    course = request.form['txtcourse'])
                 session.add(newItem)
                 session.commit()
@@ -147,6 +147,100 @@ def showLogin():
         login_session['state'] = state
         return render_template('login.html',STATE = state)
 
+# a method to call the facebook server to run the api
+@app.route('/fbconnect',methods=['POST'])
+def fbconnect():
+        
+        if request.args.get('state') != login_session['state']:
+                response = make_response(json.dumps('Invalid state parameter.'), 401)
+                response.headers['Content-Type'] = 'application/json'
+                return response
+        access_token = request.data
+
+        app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+        app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+        h = httplib2.Http()
+        result = h.request(url, 'GET')[1]
+
+         # Use token to get user info from API
+        userinfo_url = "https://graph.facebook.com/v2.8/me"
+        '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+        '''
+        token = result.split(',')[0].split(':')[1].replace('"', '')
+
+        url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+        h = httplib2.Http()
+        result = h.request(url, 'GET')[1]
+        # print "url sent for API access:%s"% url
+        output = "API JSON result: %s" % result
+        
+        
+        data = json.loads(result)
+        login_session['provider'] = 'facebook'
+        login_session['username'] = data["name"]
+        #login_session['email'] = data["email"]
+        login_session['facebook_id'] = data["id"]
+        
+        # The token must be stored in the login_session in order to properly logout
+        login_session['access_token'] = token
+
+        # Get user picture
+        url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+        h = httplib2.Http()
+        result = h.request(url, 'GET')[1]
+        data = json.loads(result)
+
+        login_session['picture'] = data["data"]["url"]
+
+        # see if user exists
+        '''
+        user_id = getUserID(login_session['email'])
+        if not user_id:
+        '''
+        user_id = createUser(login_session)
+        login_session['user_id'] = user_id
+        
+        
+        output = ''
+        output += '<h1>Welcome, '
+        output += login_session['username']
+
+        output += '!</h1>'
+        output += '<img src="'
+        output += login_session['picture']
+        output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+        flash("Now logged in as %s" % login_session['username'])
+        return output
+
+# Method to call FB server to disconnect and delete the token
+@app.route('/fbdisconnect')
+def fbdisconnect():
+        facebook_id = login_session['facebook_id']
+        access_token = login_session['access_token']
+        url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+        h = httplib2.Http()
+        result = h.request(url, 'DELETE')[1]
+        del login_session['provider'] 
+        del login_session['username'] 
+        del login_session['facebook_id'] 
+        del login_session['access_token'] 
+        del login_session['picture']
+        del login_session['user_id'] 
+        return "you have been logged out"
+    
+
+
+# a method to call the google server to run the api
 @app.route('/gconnect',methods=['POST'])
 def gconnect():
     # Validate state token
@@ -215,7 +309,7 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
-
+    login_session['provider'] = 'google'
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
@@ -243,11 +337,12 @@ def gconnect():
     return output
 
 def createUser(login_session):
-        newUser = User(name=login_session['username'],email=login_session['email'],
+        newUser = User(name=login_session['username'],
+                       email=login_session['username']+"@yahoo.com",
                        picture=login_session['picture'])
         session.add(newUser)
         session.commit()
-        user = session.query(User).filter_by(email=login_session['email']).one()
+        user = session.query(User).filter_by(id=newUser.id).one()
         return user.id
 
 def getUserInfo(user_id):
@@ -262,8 +357,17 @@ def getUserId(email):
                 return None
 
         
-
-
+        
+@app.route("/logout")
+def logout():
+        if login_session['provider'] == "google":
+                gdisconnect()
+        if login_session['provider'] == 'facebook':
+                fbdisconnect()
+        flash("you have been successfully logged out!!")
+        return redirect(url_for('showLogin'))
+                
+                
                 
 @app.route("/gdisconnect")
 def gdisconnect():
